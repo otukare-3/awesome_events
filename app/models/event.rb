e881848e272bed08b8fd5bbf53c961b0b97c007b
Takuya Wada <sayusayusayusayu95gmai.com>
2021-06-15 17:35:43 +0900

    イベント詳細ページに編集ボタンを追加 p320

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,13 +7,19 @@ class Event < ApplicationRecord
   validates :end_at, presence: true
   validate :start_at_should_be_before_end_at
 
+  def created_by?(user)
+    return false unless user
+
+    owner_id == user.id
+  end
+
   private
 
   def start_at_should_be_before_end_at
     return unless start_at && end_at
 
     if start_at >= end_at
-      errors.add(:start_at, "は終了時間よりも前に設定してください")
+      errors.add(:start_at, 'は終了時間よりも前に設定してください')
     end
   end
 end
